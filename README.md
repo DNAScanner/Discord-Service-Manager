37d78b6a85223d8a19afbe89ac6e58a2099beaef
DNAScanner <main@dnascanner.de>
2024-06-25 18:41:12 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Discord Service Manager for Linux by DNA
+
+This is a [Deno](https://deno.com) script, which allows you to manage the services on your Linux machine with ease
+
+## Screenshots
+
+![alt text](docs/1.svg)
+![alt text](docs/2.svg)
+![alt text](docs/3.svg)
+
+## Setup
+
+1. Clone the repository
+2. Install [Deno](https://deno.com/)
+3. Rename `env.template` to `.env` and fill in the required fields
+4. Run `deno run -A main.ts`
---
+++ main.ts
@@ -0,0 +1,225 @@
+import {Client, Events} from "npm:discord.js";
+import {Routes} from "npm:discord-api-types/v9";
+import {} from "npm:@discordjs/brokers";
+import {SlashCommandBuilder, ActionRowBuilder, ButtonBuilder} from "npm:@discordjs/builders";
+import {} from "npm:@discordjs/collection";
+import {} from "npm:@discordjs/core";
+import {} from "npm:@discordjs/formatters";
+import {} from "npm:@discordjs/proxy";
+import {REST} from "npm:@discordjs/rest";
+import {} from "npm:@discordjs/voice";
+import {} from "npm:@discordjs/util";
+import {} from "npm:@discordjs/ws";
+
+import {load} from "https://deno.land/std@0.224.0/dotenv/mod.ts";
+import {ButtonStyle} from "npm:discord-api-types/v10";
+
+type AwaitMessageComponentResult = {
+	readonly deferUpdate: () => Promise<void>;
+	readonly customId: string;
+};
+
+const env = await load({export: true});
+
+const rest = new REST().setToken(env.TOKEN);
+
+const client = new Client({intents: []});
+
+client.on(Events.ClientReady, (client) => {
+	console.log(`Logged in as ${client.user?.tag}`);
+
+	// Register commands
+	(async () => {
+		try {
+			console.log("Registering commands");
+
+			await rest.put(Routes.applicationCommands(client.user.id), {
+				body: [
+					new SlashCommandBuilder() //
+						.setName("service")
+						.setDescription("Manage a specified service")
+						.addStringOption((option) =>
+							option //
+								.setName("service")
+								.setDescription("The service to manage")
+								.setRequired(true)
+						)
+						.addBooleanOption((option) =>
+							option //
+								.setName("public")
+								.setDescription("Show the panel to everyone")
+						),
+				],
+			});
+
+			console.log("Commands registered");
+		} catch (error) {
+			console.error(error);
+		}
+	})();
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isCommand()) return;
+
+	const {commandName} = interaction;
+
+	switch (commandName) {
+		case "service": {
+			const serviceName = interaction.options.get("service")?.value as string;
+			const isPublic = interaction.options.get("public")?.value as boolean;
+
+			await interaction.reply({ephemeral: !isPublic, content: `### Loading data for service \`${serviceName}\``});
+
+			while (true) {
+				const status = await service.getStatus(serviceName);
+
+				if (status === "Service not found") {
+					await interaction.editReply({content: `### Service \`${serviceName}\` not found`});
+					break;
+				}
+
+				const serviceActive = status.includes("Active: active");
+
+				const response = await interaction.editReply({
+					content: `### ${serviceActive ? "🟢" : "🔴"} Managing Service \`${serviceName}\`\n\n\`\`\`\n` + status + "\n```",
+					components: [
+						//
+						new ActionRowBuilder() //
+							.addComponents(
+								new ButtonBuilder() // Update status
+									.setStyle(ButtonStyle.Primary)
+									.setLabel("Update Status")
+									.setCustomId("update")
+							)
+							.addComponents(
+								isPublic
+									? [
+											new ButtonBuilder() // Dismiss
+												.setStyle(ButtonStyle.Secondary)
+												.setLabel("Dismiss")
+												.setCustomId("dismiss"),
+									  ]
+									: []
+							),
+
+						new ActionRowBuilder() //
+							.addComponents(
+								//
+								new ButtonBuilder() // Start
+									.setStyle(ButtonStyle.Success)
+									.setLabel("Start")
+									.setCustomId("start")
+									.setDisabled(serviceActive),
+
+								new ButtonBuilder() // Stop
+									.setStyle(ButtonStyle.Danger)
+									.setLabel("Stop")
+									.setCustomId("stop")
+									.setDisabled(!serviceActive),
+
+								new ButtonBuilder() // Restart
+									.setStyle(ButtonStyle.Primary)
+									.setLabel("Restart")
+									.setCustomId("restart")
+									.setDisabled(!serviceActive)
+							),
+					],
+				});
+
+				// deno-lint-ignore no-explicit-any
+				const filter = (i: any) => i.user.id === interaction.user.id;
+
+				// Wait for user input
+				try {
+					const choice = (await response.awaitMessageComponent({
+						filter,
+						time: 300000,
+					})) as AwaitMessageComponentResult;
+
+					await choice.deferUpdate();
+
+					let exit = false;
+
+					switch (choice.customId) {
+						case "update": {
+							break;
+						}
+
+						case "dismiss": {
+							await interaction.deleteReply();
+							exit = true;
+							break;
+						}
+
+						case "start": {
+							await interaction.editReply({content: `### Starting service \`${serviceName}\`...`});
+							await service.start(serviceName);
+							break;
+						}
+
+						case "stop": {
+							await interaction.editReply({content: `### Stopping service \`${serviceName}\`...`});
+							await service.stop(serviceName);
+							break;
+						}
+
+						case "restart": {
+							await interaction.editReply({content: `### Restarting service \`${serviceName}\`...`});
+							await service.restart(serviceName);
+							break;
+						}
+
+						default: {
+							await interaction.editReply({content: choice.customId + " is not handled yet"});
+							break;
+						}
+					}
+
+					if (exit) break;
+				} catch (error) {
+					await interaction.editReply({content: "```ts\n" + error + "\n```"});
+					break;
+				}
+			}
+
+			break;
+		}
+	}
+});
+
+client.login(env.TOKEN);
+
+const service = {
+	getStatus: async (serviceName: string): Promise<string> =>
+		await new Promise((resolve) => {
+			const output = new Deno.Command("systemctl", {args: ["status", serviceName]}).outputSync();
+
+			if (output.code !== 4) {
+				resolve(new TextDecoder().decode(output.stdout));
+			} else {
+				resolve("Service not found");
+			}
+		}),
+
+	start: async (serviceName: string): Promise<void> =>
+		await new Promise((resolve) => {
+			new Deno.Command("systemctl", {args: ["start", serviceName]}).outputSync();
+
+			resolve();
+		}),
+
+	stop: async (serviceName: string): Promise<void> =>
+		await new Promise((resolve) => {
+			new Deno.Command("systemctl", {args: ["stop", serviceName]}).outputSync();
+
+			resolve();
+		}),
+
+	restart: async (serviceName: string): Promise<void> =>
+		await new Promise((resolve) => {
+			new Deno.Command("systemctl", {args: ["restart", serviceName]}).outputSync();
+
+			resolve();
+		}),
+};
